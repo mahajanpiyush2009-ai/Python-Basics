893c16c32948cb33750d2b8ecefc9e25443abf63
Piyush <mahajanpiyush20090213@gmail.com>
2026-06-27 12:41:34 +0530

    first commit

---
+++ Set.py
@@ -0,0 +1,17 @@
+number_set ={10,20,30,40,50}
+#print(number_set)
+duplicate_set = {1,2,2,3,3,4}
+#print(duplicate_set)
+empty_set = set()
+#print(empty_set)
+my_set ={1,2,4,2,5,6}
+my_set.difference_update({1,4})
+#print(my_set)
+my_dict = {"name": "John","age": 21,"role": "admin"}
+##print(my_dict["age"])
+my_dict = {"brand": "BMW", "model":"M5","year":"2008"}
+print(my_dict.items())
+ 
+
+
+# list[],tuple(),set&dictionary{},    
---
+++ first .py
@@ -0,0 +1,24 @@
+fruits=["Apple","Banana","Cherry","Dates"]
+#print(fruits)
+lst = [10,20,30,40,50]
+#print("First element:",lst[0])
+#print("Last element:",lst[-1])
+#print("length of list:", len(lst))
+numbers=[]
+numbers.append(20)
+numbers.append(90)
+numbers.append(24)
+#print(numbers)
+lst=[10,20,30]
+lst.append(40)
+#print(lst)
+lst=[10,20,30,40]
+lst.insert(2,25)
+#print(lst)
+lst=["java","c++","Html"]
+lst.insert(0,"python")
+#print(lst)
+list1=[10,20,30]
+list2=[40,50,60]
+list1.extend(list2)
+print(list1)
